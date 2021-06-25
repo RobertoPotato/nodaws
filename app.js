afa3e1d010f1c6697ba71b78a062c50a3c67ff26
RobertoPotato <robertwabuti@gmail.com>
2021-06-25 11:54:18 +0300

    Changed port configuration

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Server running');
-});
-
 app.get('/', (req, res) => {
   console.log('Home is accessible');
   res.status(200).send('Welcome to the home page');
+});
+
+const port = process.env.port || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
